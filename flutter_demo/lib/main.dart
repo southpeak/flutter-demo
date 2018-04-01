b614f9c5017512552792b517460b462c4d34b922
southpeak <tech.tiny.set@gmail.com>
2018-04-01 15:32:55 +0800

    pubspec.yaml: english_words

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new FlutterDemo());
 
 class FlutterDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      
-      return new MaterialApp(
-        title: 'Welcome to Flutter',
-        home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Welcome to Flutter'),
-          ),
-          body: new Center(
-            child: new Text('Hello World'),
-          ),
+    final wordPair = new WordPair.random();
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
         ),
-      );
-    }
+        body: new Center(
+          child: new Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
 }
