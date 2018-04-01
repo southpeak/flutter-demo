d26a8978e11351c8ab5ecba9ae456f1bd33daf29
southpeak <tech.tiny.set@gmail.com>
2018-04-01 15:44:41 +0800

    StatefulWidget RandomWords

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(new FlutterDemo());
 class FlutterDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
+    
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -14,9 +14,22 @@ class FlutterDemo extends StatelessWidget {
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
